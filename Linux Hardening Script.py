527501e215e778c24cceef808d143c9335a68ac2
Jafa Yassine <jafayassine03@gmail.com>
2026-05-25 08:47:49 +0800

    Add files via upload

--- Linux Hardening Script.py
+++ Linux Hardening Script.py
@@ -1,4 +1,6 @@
 import os
+import random
+import string
 
 print("=== Simple Linux Hardening Tool ===")
 
@@ -13,7 +15,11 @@ while True:
     print("3. Disable root SSH login")
     print("4. Change SSH port")
     print("5. Create new user")
-    print("6. Exit")
+    print("6. Set password policy")
+    print("7. Install Fail2Ban")
+    print("8. Secure shared memory")
+    print("9. Generate random password")
+    print("10. Exit")
 
     choice = input("Enter choice: ")
 
@@ -58,7 +64,6 @@ while True:
 
         print(f"SSH port changed to {new_port}")
 
-    # 5. Create new user
     elif choice == "5":
         username = input("Enter username: ")
 
@@ -67,8 +72,53 @@ while True:
 
         print(f"User {username} created and added to sudo group.")
 
-    # 6. Exit
     elif choice == "6":
+        print("Setting password policy...")
+
+        os.system(
+            "sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS   90/' /etc/login.defs"
+        )
+
+        os.system(
+            "sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS   7/' /etc/login.defs"
+        )
+
+        os.system(
+            "sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE   14/' /etc/login.defs"
+        )
+
+        print("Password policy updated.")
+
+    elif choice == "7":
+        print("Installing Fail2Ban...")
+
+        os.system("apt install fail2ban -y")
+        os.system("systemctl enable fail2ban")
+        os.system("systemctl start fail2ban")
+
+        print("Fail2Ban installed and running.")
+
+    elif choice == "8":
+        print("Securing shared memory...")
+
+        os.system(
+            "echo 'tmpfs /run/shm tmpfs defaults,noexec,nosuid 0 0' >> /etc/fstab"
+        )
+
+        os.system("mount -o remount /run/shm")
+
+        print("Shared memory secured.")
+
+    elif choice == "9":
+        length = int(input("Enter password length: "))
+
+        chars = string.ascii_letters + string.digits + "!@#$%^&*"
+
+        password = "".join(random.choice(chars) for _ in range(length))
+
+        print(f"Generated Password: {password}")
+
+    elif choice == "10":
         print("Exiting...")
         break
 
